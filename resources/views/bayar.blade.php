73afe6295dc1b1e2b809c4b400fe22d65b721da1
ryanhandys <ryanhandy137@gmail.com>
2022-12-09 20:48:45 +0700

    midtrans

--- resources/views/bayar.blade.php
+++ resources/views/bayar.blade.php
@@ -2,7 +2,7 @@
 
 @section('css')
 <script type="text/javascript"
-src="https://app.sandbox.midtrans.com/snap/snap.js"
+src="https://app.midtrans.com/snap/snap.js"
 data-client-key="{{ env('MIDTRANS_CLIENT_KEY') }}"></script>
 @endsection
 
